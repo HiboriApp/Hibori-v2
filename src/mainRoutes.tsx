ab0e13859a5fcbb49884ba75909710f18e4d09ed
ori raisfeld <raisfeldori@gmail.com>
2024-12-02 12:58:21 +0200

    more pages

--- src/mainRoutes.tsx
+++ src/mainRoutes.tsx
@@ -1,11 +1,21 @@
 import { RouteObject } from "react-router-dom"
 import Home from "./main/home";
+import Connections from "./main/connections";
+import Messages from "./main/messages";
 
 export default function Routes() : RouteObject[]{
   return [
     {
       path: "/home",
       element: <Home></Home>
+    },
+    {
+      path: '/connections',
+      element: <Connections></Connections>
+    },
+    {
+      path: '/messages',
+      element: <Messages></Messages>
     }
   ];
 }
