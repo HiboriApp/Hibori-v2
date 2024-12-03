5260ff6cf6b9c27b6d6fea7e555daab4584e299a
ori raisfeld <raisfeldori@gmail.com>
2024-12-03 12:18:49 +0200

    added basic qna stuff

---
+++ src/user/help/start.tsx
@@ -0,0 +1,5 @@
+export default function Start(){
+    return <div>
+        התחלה של התוכן (כרגע ריק)
+    </div>
+}
--- src/user/qna.tsx
+++ src/user/qna.tsx
@@ -1,7 +1,14 @@
 import Layout from "../components/layout";
+import Main from './help/start';
+
+function Structure(){
+    return <div>
+        <Main></Main>
+    </div>
+}
 
 export default function Qna() {
     return <Layout
-    children={<div></div>}
+    children={<div><Structure></Structure></div>}
     ></Layout>
 }
