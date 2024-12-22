52cb1a0c52f76d6716df674e3a05b6f83a36a020
ori raisfeld <raisfeldori@gmail.com>
2024-12-22 10:21:50 +0200

    notifications are now checked if true

--- src/user/settings.tsx
+++ src/user/settings.tsx
@@ -146,7 +146,7 @@ export default function SettingsPage() {
               <div className="flex items-center justify-between">
                 <span className="text-sm font-medium">התראות דחיפה</span>
                 <label className="relative inline-flex items-center cursor-pointer">
-                  <input value={"" + user.wantsNotifications} onChange={(e) => setUser({ ...user, wantsNotifications: e.target.checked })} type="checkbox" className="sr-only peer" />
+                  <input checked={user.wantsNotifications} onChange={(e) => setUser({ ...user, wantsNotifications: e.target.checked })} type="checkbox" className="sr-only peer" />
                   <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:right-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500"></div>
                 </label>
               </div>
