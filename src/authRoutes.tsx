0078151e06ec709c3c098610d2a6a87eefc7cd3c
ori raisfeld <raisfeldori@gmail.com>
2025-03-17 08:42:38 +0200

    moved the login route

--- src/authRoutes.tsx
+++ src/authRoutes.tsx
@@ -1,11 +1,16 @@
 import { RouteObject } from "react-router-dom"
 import Signup  from "./auth/signup";
+import Login from "./auth/login";
 
 export default function Routes() : RouteObject[]{
   return [
     {
       path: '/',
       element: <Signup></Signup>
+    },
+    {
+      path: "/login",
+      element: <Login></Login>
     }
   ];
 }
