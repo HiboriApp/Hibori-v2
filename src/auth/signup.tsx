bd27114289cc9263ca6aeba2913b61a486ceadd4
ori raisfeld <raisfeldori@gmail.com>
2025-03-17 09:17:35 +0200

    updated routing

--- src/auth/signup.tsx
+++ src/auth/signup.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { SignUp } from '../api/auth';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../api/firebase';
 
@@ -129,7 +129,7 @@ export function SignUpPage(){
           </form>
           <div className="mt-6 text-center">
             <p className="text-[#115614]">כבר יש לך חשבון?</p>
-            <a href="/login" className="text-[#06AA06] font-bold hover:underline">התחבר עכשיו</a>
+            <Link to="/login" className="text-[#06AA06] font-bold hover:underline">התחבר עכשיו</Link>
           </div>
         </div>
       </div>
