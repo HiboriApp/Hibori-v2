6012e5ca12bd6241116a721fe0d255eb254a14df
ori raisfeld <raisfeldori@gmail.com>
2025-11-06 12:44:35 +0200

    Comment out hash function implementation
    Comment out the hash function in pass.tsx.

--- src/api/pass.tsx
+++ src/api/pass.tsx
@@ -1,11 +1,11 @@
-export async function hash(message: string) {
+/*export async function hash(message: string) {
   const encoder = new TextEncoder();
   const data = encoder.encode(message);
   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
   return hashHex;
-}
+}*/
 
 // A builtin password for the site, this can be configured
 export const password = import.meta.env.VITE_PASSWORD;
